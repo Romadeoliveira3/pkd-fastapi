f868719625e4b3a8808fa22ae7fb56dad5a8c552
romadeoliveira3 <romadeoliveira3@gmail.com>
2025-03-26 21:01:24 -0300

    Fix: Intropection validation implementada

--- microservice-items/auth.py
+++ microservice-items/auth.py
@@ -1,48 +1,51 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer
-from jose import jwt, JWTError
+from jose import JWTError
 from pydantic import BaseModel
 from typing import List
 import httpx
 
 oauth2_scheme = HTTPBearer()
 
-KEYCLOAK_URL = "http://keycloak:8080"
-REALM_NAME = "PKD-realm"
-INTROSPECT_URL = f"{KEYCLOAK_URL}/realms/{REALM_NAME}/protocol/openid-connect/token/introspect"
-JWKS_URL = f"{KEYCLOAK_URL}/realms/{REALM_NAME}/protocol/openid-connect/certs"
-ALGORITHM = "RS256"
+NGINX_INTROSPECT_URL = "http://nginx-gateway/auth/introspect"
 KEYCLOAK_CLIENT_ID = "confidential-client"
 KEYCLOAK_CLIENT_SECRET = "PKD-client-secret"
-
-class TokenRequest(BaseModel):
-    username: str
-    password: str
 
 class TokenData(BaseModel):
     username: str
     roles: List[str]
 
 async def validate_token_introspect(token: str) -> TokenData:
+    token = token.replace("Bearer ", "")
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = {
         "token": token,
         "client_id": KEYCLOAK_CLIENT_ID,
         "client_secret": KEYCLOAK_CLIENT_SECRET
     }
+
+    print("Enviando introspect via NGINX:")
+    print(f"token: {token[:40]}...")
+    print(f"url: {NGINX_INTROSPECT_URL}")
+
     async with httpx.AsyncClient() as client:
-        response = await client.post(INTROSPECT_URL, headers=headers, data=data)
+        response = await client.post(NGINX_INTROSPECT_URL, headers=headers, data=data)
+        print(f"Status introspect: {response.status_code}")
+        print(f"Resposta introspect: {response.text}")
+
         if response.status_code != 200:
-            raise HTTPException(status_code=500, detail="Keycloak introspection failed")
+            raise HTTPException(status_code=500, detail="Falha na introspecção com Keycloak via NGINX")
+
         result = response.json()
         if not result.get("active", False):
-            raise HTTPException(status_code=401, detail="Invalid or expired token")
+            raise HTTPException(status_code=401, detail="Token inválido ou expirado")
+
     return TokenData(
-        username=result.get("preferred_username", "unknown"),
+        username=result.get("preferred_username", "desconhecido"),
         roles=result.get("realm_access", {}).get("roles", [])
     )
 
 async def validate_token(token: str = Depends(oauth2_scheme)) -> TokenData:
     if not token or not token.credentials:
-        raise HTTPException(status_code=401, detail="Missing access token")
+        raise HTTPException(status_code=401, detail="Token de acesso ausente")
     return await validate_token_introspect(token.credentials)
